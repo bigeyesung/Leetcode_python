fd60a30213941c91bf02bcfde5d70e0e775f623d
contiliosung <sungchenhsi@gmail.com>
2019-12-31 12:33:27 +0000

    decode DP

---
+++ DecodeWays.py
@@ -0,0 +1,21 @@
+class DecodeWays:
+    def numDecodings(self, s):
+        #dp[i] = dp[i-1] if s[i] != "0"
+        #       +dp[i-2] if "09" < s[i-1:i+1] < "27"
+        if s == "": return 0
+        dp = [0 for x in range(len(s)+1)]
+        dp[0] = 1
+        for i in range(1, len(s)+1):
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
+            if i != 1 and s[i-2:i] < "27" and s[i-2:i] > "09":  #"01"ways = 0
+                dp[i] += dp[i-2]
+        return dp[len(s)] 
+
+
+def Main():
+    decode = DecodeWays()
+    ans = decode.numDecodings("222322")
+    print(ans)
+if __name__ == "__main__":
+    Main()
