39c8e9a7d9faa6d8f0550e6afd0646cb6be78d17
chenhsi <sungchenhsi@gmail.com>
2020-03-07 22:13:52 +0000

    minor change

--- IsAlien.py
+++ IsAlien.py
@@ -6,7 +6,12 @@ def isAlienSorted(words, order):
         m = {c: i for i, c in enumerate(order)}
         words = [[m[c] for c in w] for w in words]
         for w1, w2 in zip(words, words[1:]):
-            print(w1, w2)
+            print(w1)
+            print(w2)
+            test = (w1 <= w2 for w1, w2 in zip(words, words[1:]))
+            print(test)
+            ret = all(test)
+
         return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
 
 
