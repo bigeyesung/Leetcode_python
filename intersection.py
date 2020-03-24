8429ce66018bb07acff8c12778e31d82ef5b96fe
chenhsi <sungchenhsi@gmail.com>
2020-03-24 23:29:00 +0000

    -intersection.pt

---
+++ intersection.py
@@ -0,0 +1,25 @@
+def arraysIntersection(arr1,arr2,arr3):
+    res = []
+    if arr1 == None or arr2 == None or arr3 == None:
+        return res
+    i,j,k =0,0,0
+    while(i<len(arr1) and j<len(arr2) and k<len(arr3)):
+        if arr1[i]==arr2[j]==arr3[k]:
+            res.append(arr1[i])
+            i+=1
+            j+=1
+            k+=1
+        elif arr1[i]<arr2[j]:
+            i+=1
+        elif arr2[j]<arr3[k]:
+            j+=1
+        else:
+            k+=1
+    return res
+
+arr1 = [1,2,3,4,5]
+arr2 = [1,2,5,7,9]
+arr3 = [1,3,4,5,8]
+
+res = arraysIntersection(arr1,arr2,arr3)
+print(res)
