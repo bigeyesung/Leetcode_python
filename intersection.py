20c38ae3993b61bc243c7bd63f9c48333969a72e
chenhsi <sungchenhsi@gmail.com>
2020-03-26 08:44:25 +0000

    -intersection of sorted array

--- intersection.py
+++ intersection.py
@@ -1,3 +1,4 @@
+import collections
 def arraysIntersection(arr1,arr2,arr3):
     res = []
     if arr1 == None or arr2 == None or arr3 == None:
@@ -23,3 +24,12 @@ arr3 = [1,3,4,5,8]
 
 res = arraysIntersection(arr1,arr2,arr3)
 print(res)
+arr1.extend(arr2)
+arr1.extend(arr3)
+count = collections.Counter(arr1)
+print(type(count))
+ans = []
+for key in count:
+    if count[key]==3:
+        ans.append(key)
+print(ans)
